c36c04d1a37ca7a28be5a8d911800d99035721ca
Stephen <stephen.benson@cksoftware.ca>
2018-06-16 17:03:55 -0600

    Added Missing ConsoleShapeFactory Tests

---
+++ ScopeARJobApplicationTests/ConsoleShapeFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScopeARJobApplication.ConsoleShape;
+
+
+namespace ScopeARJobApplicationTests
+{
+    [TestClass]
+    public class ConsoleShapeFactoryTests
+    {
+        [TestMethod]
+        public void TestConsoleShapeFactoryCreation()
+        {
+            var factory = new ConsoleShapeFactory();
+            Assert.IsNotNull(factory);
+        }
+        [TestMethod]
+        public void TestChristmasTreeCreation()
+        {
+            var factory = new ConsoleShapeFactory();
+            var testShape = factory.GetConsoleShape(ConsoleShapeEnum.ChistmasTreeShape, 2);
+            Assert.IsNotNull(testShape);
+        }
+
+        [TestMethod]
+        public void TestXShapeCreation()
+        {
+            var factory = new ConsoleShapeFactory();
+            var testShape = factory.GetConsoleShape(ConsoleShapeEnum.XShape, 2);
+            Assert.IsNotNull(testShape);
+        }
+    }
+}
